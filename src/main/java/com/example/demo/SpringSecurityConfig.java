f0fb59f5d8224b5c85ab56013302962e19cf0880
Vy <vy.donhu@emeal.nttdata.com>
2024-12-06 08:53:02 +0700

    add h2

--- src/main/java/com/example/demo/SpringSecurityConfig.java
+++ src/main/java/com/example/demo/SpringSecurityConfig.java
@@ -8,14 +8,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurityConfig {
 
     public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                request -> {
-                    request.requestMatchers("/bar").permitAll()
-                            .requestMatchers("/bar/{bar_id}").authenticated()
-                            .requestMatchers("/bar/{bar_id}/**").authenticated()
-                            .requestMatchers("foo//foo/{foo_id}/{id2}/ack").anonymous();
-                }
-        );
+        //http.authorizeHttpRequests(
+        //        request -> {
+        //            request.requestMatchers("/bar").permitAll()
+        //                    .requestMatchers("/bar/{bar_id}").authenticated()
+        //                    .requestMatchers("/bar/{bar_id}/**").authenticated()
+        //                    .requestMatchers("foo//foo/{foo_id}/{id2}/ack").anonymous();
+        //        }
+        //);
 
         return http.build();
     }
